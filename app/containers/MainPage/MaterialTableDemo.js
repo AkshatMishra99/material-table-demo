5fee347df939a85b8fbaa9b93071de0bd9f013c1
Akshat Mishra <mailakshat99@gmail.com>
2021-08-04 19:28:01 +0530

    made changes

--- app/containers/MainPage/MaterialTableDemo.js
+++ app/containers/MainPage/MaterialTableDemo.js
@@ -61,7 +61,9 @@ const MaterialTableDemo = () => {
     Add: forwardRef((props, ref) => <AddBox {...props} ref={ref} />),
     Check: forwardRef((props, ref) => <Check {...props} ref={ref} />),
     Clear: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
-    Delete: forwardRef((props, ref) => <DeleteOutline {...props} ref={ref} />),
+    Delete: forwardRef((props, ref) => (
+      <DeleteOutline {...props} ref={ref} style={{ color: 'red' }} />
+    )),
     DetailPanel: forwardRef((props, ref) => (
       <ChevronRight {...props} ref={ref} />
     )),
